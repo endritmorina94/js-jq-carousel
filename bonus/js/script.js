025b50af9462da68bd5dbc032ff6d00c0cae7eeb
endritmorina94 <endrit.morina@outlook.it>
2021-04-21 01:02:16 +0200

    aggiunto inizializzazione corretta jQuery

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,102 +1,106 @@
-//Dichiaro una variabile che indica il contenitore delle immagini
-var images = $(".images");
+$(document).ready(function(){
 
-//Rilevo l'altezza della prima immagine e la indico in una variabile
-var imgHeight = $("#prima").css("height");
+    //Dichiaro una variabile che indica il contenitore delle immagini
+    var images = $(".images");
 
-//Assegno l'altezza dell'immagine al contenitore
-$(images).css("height", imgHeight);
+    //Rilevo l'altezza della prima immagine e la indico in una variabile
+    var imgHeight = $("#prima").css("height");
 
-
-// Indico la freccia destra con una variabile
-var arrRight = $(".arrow.right");
-
-//Creo una funzione che si attiva al click della freccia destra
-arrRight.click(function() {
-
-    //Dichiaro una variabile che inidica l'immagine attualmente visibile
-    var currentImg = $(".images .displayed");
-
-    var currentBtn = $(".btn.active-btn");
-
-    //Le tolgo la classe displayed, quindi la nascondo
-    currentImg.removeClass("displayed").fadeOut("slow");
-
-    currentBtn.removeClass("active-btn");
-
-    //Indico l'immagine dopo con una variabile
-    var nextImg = currentImg.next();
-
-    var nextBtn = currentBtn.next();
-
-    //Se l'immagine attuale ha la classe last ed è quindi l'ultima
-    //tornerà visibile la prima immagine della lista
-    if (currentImg.hasClass("last")) {
-
-        $(".images .first").addClass("displayed").fadeIn("slow");
-
-        $(".btn.first").addClass("active-btn");
-
-    //Altrimenti do la classe displayed (rendo quindi visibile), l'immagine dopo
-    } else {
-
-        nextImg.addClass("displayed").fadeIn("slow");
-
-        nextBtn.addClass("active-btn");
-
-    }
-
-});
+    //Assegno l'altezza dell'immagine al contenitore
+    $(images).css("height", imgHeight);
 
 
-// DI SEGUITO: TUTTO QUELLO CHE C'è SCRITTO SOPRA MA AL CONTRARIO
-//PER SLIDEARE LE IMMAGINI ALL'INDIETRO ANZICHè IN AVANTI
-var arrLeft = $(".arrow.left");
+    // Indico la freccia destra con una variabile
+    var arrRight = $(".arrow.right");
 
-arrLeft.click(function() {
+    //Creo una funzione che si attiva al click della freccia destra
+    arrRight.click(function() {
 
-    var currentImg = $(".images .displayed");
-    var currentBtn = $(".btn.active-btn");
+        //Dichiaro una variabile che inidica l'immagine attualmente visibile
+        var currentImg = $(".images .displayed");
 
-    currentImg.removeClass("displayed").fadeOut("slow");
-    currentBtn.removeClass("active-btn");
+        var currentBtn = $(".btn.active-btn");
+
+        //Le tolgo la classe displayed, quindi la nascondo
+        currentImg.removeClass("displayed").fadeOut("slow");
+
+        currentBtn.removeClass("active-btn");
+
+        //Indico l'immagine dopo con una variabile
+        var nextImg = currentImg.next();
+
+        var nextBtn = currentBtn.next();
+
+        //Se l'immagine attuale ha la classe last ed è quindi l'ultima
+        //tornerà visibile la prima immagine della lista
+        if (currentImg.hasClass("last")) {
+
+            $(".images .first").addClass("displayed").fadeIn("slow");
+
+            $(".btn.first").addClass("active-btn");
+
+        //Altrimenti do la classe displayed (rendo quindi visibile), l'immagine dopo
+        } else {
+
+            nextImg.addClass("displayed").fadeIn("slow");
+
+            nextBtn.addClass("active-btn");
+
+        }
+
+    });
 
 
-    var prevImg = currentImg.prev();
-    var prevBtn = currentBtn.prev();
+    // DI SEGUITO: TUTTO QUELLO CHE C'è SCRITTO SOPRA MA AL CONTRARIO
+    //PER SLIDEARE LE IMMAGINI ALL'INDIETRO ANZICHè IN AVANTI
+    var arrLeft = $(".arrow.left");
 
-    if (currentImg.hasClass("first")) {
+    arrLeft.click(function() {
 
-        $(".images .last").addClass("displayed").fadeIn("slow");
-        $(".btn.last").addClass("active-btn");
+        var currentImg = $(".images .displayed");
+        var currentBtn = $(".btn.active-btn");
 
-    } else {
-
-        prevImg.addClass("displayed").fadeIn("slow");
-        prevBtn.addClass("active-btn");
-
-    }
-
-});
+        currentImg.removeClass("displayed").fadeOut("slow");
+        currentBtn.removeClass("active-btn");
 
 
-// BONUS: BOTTONI DELLO SLIDER
+        var prevImg = currentImg.prev();
+        var prevBtn = currentBtn.prev();
 
-//Aggiungo una funzione che si attiva al click di uno dei bottoni
-$(".btn").on("click", function() {
+        if (currentImg.hasClass("first")) {
 
-    //Stabilisco una variabile che indica l'indice del bottone appena cliccato
-    var index = $(this).index();
+            $(".images .last").addClass("displayed").fadeIn("slow");
+            $(".btn.last").addClass("active-btn");
 
-    //Tolgo la classe active-btn al bottone che la aveva
-    $(".btn.active-btn").removeClass("active-btn");
+        } else {
 
-    //Do la classe active-btn al bottone che è stato appena cliccato
-    $(this).addClass("active-btn");
+            prevImg.addClass("displayed").fadeIn("slow");
+            prevBtn.addClass("active-btn");
 
-    //Tolgo la classe displayed all'immagine che la aveva
-    $(".images .displayed").removeClass("displayed").fadeOut("slow");
+        }
 
-    //Do la classe displayed all'immagine che ha lo stesso indice del bottone appena cliccato
-    $("img").eq(index).addClass("displayed").fadeIn("slow");
+    });
+
+
+    // BONUS: BOTTONI DELLO SLIDER
+
+    //Aggiungo una funzione che si attiva al click di uno dei bottoni
+    $(".btn").on("click", function() {
+
+        //Stabilisco una variabile che indica l'indice del bottone appena cliccato
+        var index = $(this).index();
+
+        //Tolgo la classe active-btn al bottone che la aveva
+        $(".btn.active-btn").removeClass("active-btn");
+
+        //Do la classe active-btn al bottone che è stato appena cliccato
+        $(this).addClass("active-btn");
+
+        //Tolgo la classe displayed all'immagine che la aveva
+        $(".images .displayed").removeClass("displayed").fadeOut("slow");
+
+        //Do la classe displayed all'immagine che ha lo stesso indice del bottone appena cliccato
+        $("img").eq(index).addClass("displayed").fadeIn("slow");
+    })
+
 })
--- js/script.js
+++ js/script.js
@@ -1,54 +1,58 @@
-// Indico la freccia destra con una variabile
-var arrRight = $(".arrow.right");
+$(document).ready(function(){
 
-//Creo una funzione che si attiva al click della freccia destra
-arrRight.click(function() {
+    // Indico la freccia destra con una variabile
+    var arrRight = $(".arrow.right");
 
-    //Dichiaro una variabile che inidica l'immagine attualmente visibile
-    var currentImg = $(".images .active");
+    //Creo una funzione che si attiva al click della freccia destra
+    arrRight.click(function() {
 
-    //Le tolgo la classe active, quindi la nascondo
-    currentImg.removeClass("active");
+        //Dichiaro una variabile che inidica l'immagine attualmente visibile
+        var currentImg = $(".images .active");
 
-    //Indico l'immagine dopo con una variabile
-    var nextImg = currentImg.next();
+        //Le tolgo la classe active, quindi la nascondo
+        currentImg.removeClass("active");
 
-    //Se l'immagine attuale ha la classe last ed è quindi l'ultima
-    //tornerà visibile la prima immagine della lista
-    if (currentImg.hasClass("last")){
+        //Indico l'immagine dopo con una variabile
+        var nextImg = currentImg.next();
 
-        $(".images .first").addClass("active");
+        //Se l'immagine attuale ha la classe last ed è quindi l'ultima
+        //tornerà visibile la prima immagine della lista
+        if (currentImg.hasClass("last")){
 
-    //Altrimenti do la classe active (rendo quindi visibile), l'immagine dopo
-    } else {
+            $(".images .first").addClass("active");
 
-        nextImg.addClass("active");
+        //Altrimenti do la classe active (rendo quindi visibile), l'immagine dopo
+        } else {
 
-    }
+            nextImg.addClass("active");
 
-});
+        }
+
+    });
 
 
-// DI SEGUITO: TUTTO QUELLO CHE C'è SCRITTO SOPRA MA AL CONTRARIO
-//PER SLIDEARE LE IMMAGINI ALL'INDIETRO ANZICHè IN AVANTI
-var arrLeft = $(".arrow.left");
+    // DI SEGUITO: TUTTO QUELLO CHE C'è SCRITTO SOPRA MA AL CONTRARIO
+    //PER SLIDEARE LE IMMAGINI ALL'INDIETRO ANZICHè IN AVANTI
+    var arrLeft = $(".arrow.left");
 
-arrLeft.click(function() {
+    arrLeft.click(function() {
 
-    var currentImg = $(".images .active");
+        var currentImg = $(".images .active");
 
-    currentImg.removeClass("active");
+        currentImg.removeClass("active");
 
-    var prevImg = currentImg.prev();
+        var prevImg = currentImg.prev();
 
-    if (currentImg.hasClass("first")){
+        if (currentImg.hasClass("first")){
 
-        $(".images .last").addClass("active");
+            $(".images .last").addClass("active");
 
-    } else {
+        } else {
 
-        prevImg.addClass("active");
+            prevImg.addClass("active");
 
-    }
+        }
 
-});
+    });
+
+})
